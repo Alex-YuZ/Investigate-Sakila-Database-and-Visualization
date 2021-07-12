23b31a6aa2d91d09dde3498b96af480c8b27bc5a
Alex-Yu <alexyuzhu@gmail.com>
2021-07-13 00:16:41 +0800

    feat: Add query 2 for rental duration in quartile analysis for family movies

--- query.sql
+++ query.sql
@@ -1,4 +1,4 @@
--- QUERY 1
+/* QUERY 1 - query used for 1st insight */ 
 -- We want to understand more about the movies that families are watching. The following categories are considered family movies: Animation, Children, Classics, Comedy, Family and Music. Create a query that lists each movie, the film category it is classified in, and the number of times it has been rented out.
 
 WITH sub1 AS (
@@ -19,3 +19,23 @@ SELECT title AS film_title,
   FROM sub1
  GROUP BY 1, 2
  ORDER BY 2, 1
+
+ /* QUERY 2 - query used for 2nd insight */ 
+-- Now we need to know how the length of rental duration of these family-friendly movies compares to the duration that all movies are rented for. Can you provide a table with the movie titles and divide them into 4 levels (first_quarter, second_quarter, third_quarter, and final_quarter) based on the quartiles (25%, 50%, 75%) of the rental duration for movies across all categories? Make sure to also indicate the category that these family-friendly movies fall into.
+
+WITH sub AS (
+  SELECT title,
+         name,
+         rental_duration
+    FROM film f
+    JOIN film_category fc ON f.film_id=fc.film_id
+    JOIN category c ON c.category_id=fc.category_id
+   WHERE name in ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music')
+)
+
+SELECT title film_title,
+       name category_name,
+       rental_duration,
+       NTILE(4) OVER (ORDER BY rental_duration) AS quartile_level
+  FROM sub
+ ORDER BY quartile_level;
